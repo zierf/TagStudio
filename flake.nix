035bbfb93a09e2137995c6cda8ce22ec763a0fa2
Florian Zier <9168602+zierf@users.noreply.github.com>
2024-09-01 15:12:27 +0200

    cleanup some dependencies, reduces footprint for users of this Flake
    - use only python312 instead of python312Full
    - disable pinned nixpkgs-qt6 for now, as PySide6 is used with QT 6.7.2
    - Python dependency PySide6 includes PySide6-Addons and PySide6-Essentials
    - use "eachSystem" helper instead of relying on flake-parts and flake-utils

--- flake.nix
+++ flake.nix
@@ -2,40 +2,35 @@
   description = "TagStudio";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Pinned to Qt version 6.7.1
-    nixpkgs-qt6.url = "github:NixOS/nixpkgs/e6cea36f83499eb4e9cd184c8a8e823296b50ad5";
+    # nixpkgs-qt6.url = "github:NixOS/nixpkgs/e6cea36f83499eb4e9cd184c8a8e823296b50ad5";
 
-    systems.url = "github:nix-systems/default-linux";
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
+    supportedSystems.url = "github:nix-systems/default-linux";
 
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @inputs:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... } @inputs:
     # TODO https://github.com/NixOS/templates/blob/master/python/flake.nix
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (nixpkgs) lib;
+    let
+      # see examples for Nix Flake template and basic usage for nix-systems
+      # https://github.com/nix-systems/nix-systems?tab=readme-ov-file#basic-usage
+      # https://github.com/NixOS/templates/blob/master/python/flake.nix
+      eachSystem = nixpkgs.lib.genAttrs (import inputs.supportedSystems);
 
-        qt6Pkgs = import inputs.nixpkgs-qt6 { inherit system; };
+      pkgs = eachSystem (system: nixpkgs.legacyPackages.${system}.extend poetry2nix.overlays.default);
+      #qt6Pkgs = eachSystem (system: import inputs.nixpkgs-qt6 { inherit system; });
 
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system}.extend inputs.poetry2nix.overlays.default;
+      # override some missing dependencies for python packages
+      # https://github.com/nix-community/poetry2nix/blob/8ffbc64abe7f432882cb5d96941c39103622ae5e/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
+      pypkgs-build-requirements = {
+        #pyside6 = [ "pyside6-addons" "pyside6-essentials" ];
+      };
 
-        # override some missing dependencies for python packages
-        # https://github.com/nix-community/poetry2nix/blob/8ffbc64abe7f432882cb5d96941c39103622ae5e/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
-        pypkgs-build-requirements = {
-          #python-xlib = [ "setuptools" "setuptools-scm" ];
-        };
-        p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
+      p2n-overrides = eachSystem (system:
+        pkgs.${system}.poetry2nix.defaultPoetryOverrides.extend (self: super:
           builtins.mapAttrs
             (package: build-requirements:
               (builtins.getAttr package super).overridePythonAttrs (old: {
@@ -43,11 +38,13 @@
               })
             )
             pypkgs-build-requirements
-        );
+        )
+      );
 
-        inherit (inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      mkPoetryApplication = eachSystem (system: (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }).mkPoetryApplication);
 
-        tagstudioApp = mkPoetryApplication rec {
+      tagstudioApp = eachSystem (system:
+        mkPoetryApplication.${system} rec {
           projectDir = self;
 
           # editablePackageSources = {
@@ -56,7 +53,7 @@
 
           exeName = "tagstudio";
 
-          python = pkgs.python312Full;
+          python = pkgs.${system}.python312;
           dontWrapQtApps = false;
 
           # preferWheels to resolve typing stubs error for opencv-python:
@@ -64,7 +61,7 @@
           # https://github.com/opencv/opencv-python/issues/1010
           preferWheels = true;
 
-          overrides = p2n-overrides.extend
+          overrides = p2n-overrides.${system}.extend
             # https://github.com/nix-community/poetry2nix/blob/7619e43/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
             (final: prev: {
               pyside6-essentials = prev.pyside6-essentials.overridePythonAttrs (old: {
@@ -72,7 +69,7 @@
                 dontWrapQtApps = true;
 
                 # satisfy some missing libraries for auto-patchelf patching PySide6-Essentials
-                buildInputs = (old.buildInputs ++ (with pkgs; [
+                buildInputs = (old.buildInputs ++ (with pkgs.${system}; [
                   qt6.qtquick3d
                   qt6.qtvirtualkeyboard
                   qt6.qtwebengine
@@ -85,10 +82,11 @@
             # "pyside6"
           ];
 
-          dependencies = (with pkgs; [
-            # TODO only pyproject.toml PySide6 package fails to import PySide6.QtCore while execution
+          dependencies = (with pkgs.${system}; [
+            # TODO only pyproject.toml PySide6 package fails to import PySide6.QtWidgets while execution
             # PySide6 Nix Package should match the same version, resolving issue would allow to use arbitrary versions
-            python312Full.pkgs.pyside6
+            python312.pkgs.pyside6
+            #python312.pkgs.shiboken6
 
             dbus
             fontconfig
@@ -109,37 +107,40 @@
             # QT 6.7.0
             # zstd
           ])
-          ++ (with pkgs; [
+          ++ (with pkgs.${system}; [
             # `qt6.full` prevents error with different QT version on KDE systems:
             #   'Cannot mix incompatible Qt library (6.7.2) with this library (6.7.1)'
             qt6.full
           ]);
 
-          buildInputs = with pkgs; [
+          buildInputs = with pkgs.${system}; [
             qt6.qtbase
           ] ++ dependencies;
 
-          nativeBuildInputs = (with pkgs; [
+          nativeBuildInputs = (with pkgs.${system}; [
             copyDesktopItems
             makeWrapper
           ])
-          ++ (with pkgs; [
+          ++ (with pkgs.${system}; [
             qt6.wrapQtAppsHook
           ])
           ++ dependencies;
 
-          propogatedBuildInputs = with pkgs; [ ] ++ dependencies;
+          propogatedBuildInputs = with pkgs.${system}; [ ] ++ dependencies;
 
-          libraryPath = lib.makeLibraryPath (with pkgs; [
+          libraryPath = pkgs.${system}.lib.makeLibraryPath (with pkgs.${system}; [
             "$out"
           ] ++ dependencies);
 
-          binaryPath = lib.makeBinPath (with pkgs; [
+          binaryPath = pkgs.${system}.lib.makeBinPath (with pkgs.${system}; [
             "$out"
           ] ++ dependencies);
+
+          #LD_LIBRARY_PATH = libraryPath;
+          #PATH = binaryPath;
 
           desktopItems = [
-            (pkgs.makeDesktopItem {
+            (pkgs.${system}.makeDesktopItem {
               name = "TagStudio";
               desktopName = "TagStudio";
               comment = "A User-Focused Document Management System";
@@ -155,46 +156,49 @@
             mkdir -p $out/share/icons/hicolor/256x256/apps
             cp -rv ${projectDir}/tagstudio/resources/icon.png $out/share/icons/hicolor/256x256/apps/${exeName}.png
           '';
-        };
-      in
-      {
-        # $> nix run .
-        # apps.default = {
-        #   type = "app";
-        #   # name in [tool.poetry.scripts] section of pyproject.toml
-        #   program = "${tagstudioApp}/bin/${tagstudioApp.exeName}";
-        # };
+        }
+      );
+    in
+    {
+      # $> nix run
+      # apps.default = {
+      #   type = "app";
+      #   # name in [tool.poetry.scripts] section of pyproject.toml
+      #   program = "${tagstudioApp}/bin/${tagstudioApp.exeName}";
+      # };
 
-        # $> nix build .
-        packages = {
-          tagstudio = tagstudioApp;
-          default = tagstudioApp;
-        };
+      # $> nix run
+      packages = eachSystem (system: {
+        tagstudio = tagstudioApp.${system};
+        default = tagstudioApp.${system};
+      });
 
-        # Development Shell including `poetry` and `qtcreator`.
-        # $> nix develop
-        #
-        # Use this shell for developing the application and
-        # making changes to `pyproject.toml` and `poetry.lock` files.
-        #
-        # $> poetry install                   => generate an inital lock file
-        # $> poetry lock                      => update lock file after changing dependencies
-        # $> python ./tagstudio/tag_studio.py => launch application
-        # $> poetry run tagstudio             => execute the application with poetry
-        devShells.default = pkgs.mkShell rec {
+      # Development Shell including `poetry` and `qtcreator`.
+      # $> nix develop
+      #
+      # Use this shell for developing the application and
+      # making changes to `pyproject.toml` and `poetry.lock` files.
+      #
+      # $> poetry install                   => generate an inital lock file
+      # $> poetry lock                      => update lock file after changing dependencies
+      # $> python ./tagstudio/tag_studio.py => launch application
+      # $> poetry run tagstudio             => execute the application with poetry
+      devShells = eachSystem (system: {
+        default = pkgs.${system}.mkShell rec {
           inputsFrom = [ self.packages.${system}.tagstudio ];
 
-          packages = with pkgs; [
+          packages = with pkgs.${system}; [
             poetry
             cmake
             mypy
             ruff
-          ] ++ (with pkgs; [
+          ] ++ (with pkgs.${system}; [
             qtcreator
           ])
-          ++ tagstudioApp.dependencies;
+          ++ tagstudioApp.${system}.dependencies;
 
-          LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+          LD_LIBRARY_PATH = pkgs.${system}.lib.makeLibraryPath packages;
         };
       });
+    };
 }
